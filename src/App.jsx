95dd2a3bbf67e288159b17d821dbe9374cbf571a
Hemanth Sai <nagiseishiro@Hemanths-MacBook-Air.local>
2025-03-22 22:22:04 +0530

    changes in app,main,sockets,hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import EventPage from "./pages/EventPage";
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event/:eventId" element={<EventPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
