e0041cd27fb1735d456d4eed51c678e35ef1dea6
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-03-23 00:23:20 +0530

    navbar almost completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
-// src/components/Navbar.jsx
 import { Link } from "react-router-dom";
 import React from "react";
+import logo from "/images/feedback_logo.avif";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <img className="logo" src ="../images/feedback_logo.avif" alt="feedback" />
-        <div>
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/dashboard">Dashboard</Link>
+        <img className="logo" src={logo} alt="feedback" />
+        <div className="nav">
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/dashboard">Dashboard</Link>
+          </div>
+          <div className="nav_end">
+            <i className="fa-solid fa-user nav_icons"></i>
+          </div>
         </div>
-        </div>
-        
-
       </div>
     </nav>
   );
